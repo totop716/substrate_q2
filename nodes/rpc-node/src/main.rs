14a4e8a2c5137c53da1ea1cbf34667dd3900fffa
totop716 <totop716@gmail.com>
2022-01-25 16:47:44 -0500

    initial commit

---
+++ nodes/rpc-node/src/main.rs
@@ -0,0 +1,16 @@
+//! RPC Node Template CLI library.
+#![warn(missing_docs)]
+#![warn(unused_extern_crates)]
+
+mod chain_spec;
+#[macro_use]
+mod service;
+mod cli;
+mod command;
+mod rpc;
+mod silly_rpc;
+mod get_blocktime;
+
+fn main() -> sc_cli::Result<()> {
+	command::run()
+}
